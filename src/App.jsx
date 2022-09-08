eb1b2425bcbba29ffaba900b8dd9f9601a2de5f2
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-08 10:00:19 +0100

    search by name

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import CardContainer from './components/CardContainer/CardContainer';
+import SearchBar from './components/SearchBar/SearchBar';
+
+import { useState } from 'react';
 
 import team from './data/team';
 
 function App() {
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInput = (e) => {
+    setSearchValue(e.target.value.toLowerCase());
+  }
+
   return (
     <div className="app">
       <Header />
-      <CardContainer teamArr={team} />
+      <SearchBar handleInput={handleInput}  />
+      <CardContainer teamArr={team.filter((a) => a.name.toLowerCase().includes(searchValue))} />
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./SearchBar.scss"
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const {handleInput} = props;
   return (
-    <div>SearchBar</div>
+    <div className='search-bar'>
+      <label htmlFor="search">Search:</label>
+      <input type="text" id='search' className='search-bar__search-box' placeholder='Search by name' onChange={handleInput} />
+      {/* <label htmlFor="role">Role:</label>
+      <select id="role">
+
+      </select> */}
+    </div>
   )
 }
 
